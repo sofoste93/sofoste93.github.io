e8af407ed30b53093bb78a32be9b0f932c5f672a
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2024-03-20 16:36:24 +0100

    bug: play audio - still not working yet

--- stimme_afrikas/public/js/rehearsal-room.js
+++ stimme_afrikas/public/js/rehearsal-room.js
@@ -4,36 +4,37 @@ document.addEventListener('DOMContentLoaded', function () {
             title: 'Abram',
             key: 'F',
             tempo: '100bpm',
-            soprano: 'path/to/abram_soprano.mp3',
-            alto: 'path/to/abram_alto.mp3',
-            tenor: 'path/to/abram_tenor.mp3',
-            bass: 'path/to/abram_bass.mp3',
-            full: '../../assets/audios/mp3/Abram.mp3',
+            soprano: '../mp3/Abraham.ogg',
+            alto: '../mp3/Abraham.ogg',
+            tenor: '../mp3/Abraham.ogg',
+            bass: '../mp3/Abraham.ogg',
+            full: '../mp3/Abram.mp3',
             video: 'https://www.youtube.com/embed/FSROkdSA_ps?si=wXOVnU_5t3yF6-ty'
         },
         'louez': {
             title: 'Louez le Seigneur',
             key: 'G',
             tempo: '105bpm',
-            soprano: 'path',
-            alto: 'path',
-            tenor: 'path',
-            bass: 'path',
-            full: '../../assets/audios/mp3/Louez_le_seigneur.mp3',
+            soprano: '../mp3/Abraham.ogg',
+            alto: '../mp3/Abraham.ogg',
+            tenor: '../mp3/Abraham.ogg',
+            bass: '../mp3/Abraham.ogg',
+            full: '../mp3/Louez_le_seigneur.mp3',
             video: 'https://www.youtube.com/watch?v=T5lH_d21eH0'
         },
         'abraham': {
             title: 'Abraham',
             key: 'F',
             tempo: '100bpm',
-            soprano: 'path',
-            alto: 'path',
-            tenor: 'path',
-            bass: 'path',
-            full: '../../assets/audios/mp3/Abraham.mp3',
+            soprano: '../mp3/Abraham.ogg',
+            alto: '../mp3/Abraham.ogg',
+            tenor: '../mp3/Abraham.ogg',
+            bass: '../mp3/Abraham.ogg',
+            full: '../mp3/Abraham.ogg',
             video: "https://www.youtube.com/embed/FSROkdSA_ps?si=wXOVnU_5t3yF6-ty"
         }
     }
+
 
     const queryParams = new URLSearchParams(window.location.search);
     const songKey = queryParams.get('song');
@@ -42,11 +43,16 @@ document.addEventListener('DOMContentLoaded', function () {
     if (song) {
         document.getElementById('song-title').innerText = `${song.title} - Key: ${song.key}, Tempo: ${song.tempo}`;
         ['soprano', 'alto', 'tenor', 'bass', 'full'].forEach(part => {
-            if (document.getElementById(`${part}-audio`)) {
-                document.getElementById(`${part}-audio`).src = song[part];
+            const audioElement = document.getElementById(`${part}-audio`);
+            const sourceElement = document.getElementById(`${part}-source`);
+            const downloadLink = document.getElementById(`${part}-download`);
+            if (audioElement && sourceElement && downloadLink) {
+                sourceElement.src = song[part]; // Set the source element's src
+                audioElement.load(); // Important: Load the audio element to update the source
+                downloadLink.href = song[part]; // Set the download link's href
+                downloadLink.download = song[part].split('/').pop(); // Optional: Set the download attribute to suggest a filename
             }
         });
-        //newElement.classList.add('animate-fade-in');
         document.getElementById('video-iframe').src = song.video;
     } else {
         document.getElementById('song-details').innerHTML = '<p>Sorry, the requested song could not be found.</p>';
