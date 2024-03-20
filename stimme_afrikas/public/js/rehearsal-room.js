5dfc269589ff626390b17a1042201ccd1c43401d
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2024-03-20 15:55:27 +0100

    audio files and video link added

--- stimme_afrikas/public/js/rehearsal-room.js
+++ stimme_afrikas/public/js/rehearsal-room.js
@@ -3,16 +3,37 @@ document.addEventListener('DOMContentLoaded', function () {
         'abram': {
             title: 'Abram',
             key: 'F',
-            tempo: '95bpm',
+            tempo: '100bpm',
             soprano: 'path/to/abram_soprano.mp3',
             alto: 'path/to/abram_alto.mp3',
             tenor: 'path/to/abram_tenor.mp3',
             bass: 'path/to/abram_bass.mp3',
-            full: 'path/to/abram_full.mp3',
-            video: 'https://www.youtube.com/embed/VIDEO_ID'
+            full: '../../assets/audios/mp3/Abram.mp3',
+            video: 'https://www.youtube.com/embed/FSROkdSA_ps?si=wXOVnU_5t3yF6-ty'
         },
-        // More songs here
-    };
+        'louez': {
+            title: 'Louez le Seigneur',
+            key: 'G',
+            tempo: '105bpm',
+            soprano: 'path',
+            alto: 'path',
+            tenor: 'path',
+            bass: 'path',
+            full: '../../assets/audios/mp3/Louez_le_seigneur.mp3',
+            video: 'https://www.youtube.com/watch?v=T5lH_d21eH0'
+        },
+        'abraham': {
+            title: 'Abraham',
+            key: 'F',
+            tempo: '100bpm',
+            soprano: 'path',
+            alto: 'path',
+            tenor: 'path',
+            bass: 'path',
+            full: '../../assets/audios/mp3/Abraham.mp3',
+            video: "https://www.youtube.com/embed/FSROkdSA_ps?si=wXOVnU_5t3yF6-ty"
+        }
+    }
 
     const queryParams = new URLSearchParams(window.location.search);
     const songKey = queryParams.get('song');
--- stimme_afrikas/public/js/song-book.js
+++ stimme_afrikas/public/js/song-book.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     const songs = [
-        { title: 'Abram', key: 'F', tempo: '95bpm', link: 'rehearsal-room.html?song=abram' },
+        { title: 'Abram', key: 'F', tempo: '100bpm', link: 'rehearsal-room.html?song=abram' },
+        { title: 'Abraham', key: 'F', tempo: '100bpm', link: 'rehearsal-room.html?song=abraham' },
+        { title: 'Louez', key: 'G', tempo: '105bpm', link: 'rehearsal-room.html?song=louez' },
         { title: 'Tout est accompli', key: 'G', tempo: '70bpm', link: 'rehearsal-room.html?song=tout-est-accompli' },
         { title: 'Chris sois loué', key: 'C', tempo: '110bpm', link: 'rehearsal-room.html?song=chris-sois-loue' },
         { title: 'De peur qu\'étant rassasié', key: 'F', tempo: '95bpm', link: 'rehearsal-room.html?song=de-peur-qu-etant-rassasie' },
-        { title: 'Abrahm', key: 'F', tempo: '100bpm', link:'rehearsal-room.html?song=abraham' },
+        { title: 'Toute famille divisée', key: 'F', tempo: '100bpm', link:'rehearsal-room.html?song=toute-famille' },
         { title: "Tu m'a sauvé", key: 'A', tempo:  '105bpm', link: 'rehearsal-room.html?song=tu-m-a-sauve' },
         { title: "Pour ta bonté", key: 'F', tempo: '115bpm', link: 'rehearsal-room.html?song=pour-ta-bonte' },
         { title: "You're God alone", key: "G", tempo: "92bmp", link: "rehearsal-room.html?song=you-are-god-alone" }
